7f912393a344c079018f1876bc7e809be46fac78
Yuri <yuri.kamau@student.moringaschool.com>
2024-05-31 11:51:45 +0300

    comprehensions-generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    if (num_list):
+        num_list = [num for num in num_list if num % 2 == 0]
+        return num_list
+    else :
+        return num_list   
 
 def make_exclamation(sentence_list):
-    pass
+    if sentence_list:
+        modified_sentences = []
+        for sentence in sentence_list:
+            modified_sentence = sentence + "!"
+            modified_sentences.append(modified_sentence)
+        return modified_sentences
+    else:
+        return []
+
